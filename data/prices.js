a7550cb364b9a46d1e597ccfde07e6def8c67c67
Mel VanderWal <melvanderwal@gmail.com>
2020-09-26 18:51:14 +1000

    Scheduled prices geojson update

--- data/prices.js
+++ data/prices.js
@@ -1,7 +1,7 @@
 var data={
 	"type": "FeatureCollection",
 	"siteTime": "26/09/2020, 8:56:45 am",
-	"priceTime": "26/09/2020, 6:46:13 pm",
+	"priceTime": "26/09/2020, 6:51:13 pm",
 	"features": [
 		{
 			"type": "Feature",
@@ -15279,7 +15279,7 @@ var data={
 				"Suburb": "Toowoomba",
 				"Name": "Caltex Toowoomba Depot Front",
 				"Brand": "Caltex",
-				"LastUpdatedUtc": "2020-09-26T06:56:00",
+				"LastUpdatedUtc": "2020-09-26T07:53:00",
 				"Price": null,
 				"Overpricing": null,
 				"Marker": null,
@@ -15533,7 +15533,7 @@ var data={
 				"Suburb": "Roma",
 				"Name": "Caltex Roma Depot Front",
 				"Brand": "Caltex",
-				"LastUpdatedUtc": "2020-09-26T07:16:00",
+				"LastUpdatedUtc": "2020-09-26T07:56:00",
 				"Price": null,
 				"Overpricing": null,
 				"Marker": null,
