3c7857ce6bcf6b7f7017cfd52d42f2f3c1c106f0
Mel VanderWal <melvanderwal@gmail.com>
2020-09-29 07:36:14 +1000

    Scheduled prices geojson update

--- data/prices.js
+++ data/prices.js
@@ -1,7 +1,7 @@
 var data={
 	"type": "FeatureCollection",
 	"siteTime": "27/09/2020, 8:56:45 am",
-	"priceTime": "29/09/2020, 7:31:15 am",
+	"priceTime": "29/09/2020, 7:36:13 am",
 	"features": [
 		{
 			"type": "Feature",
