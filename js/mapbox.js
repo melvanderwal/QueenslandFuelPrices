6ec3d2cbc8f9ce6948e909b848ce26c2bcaf1f2b
melvanderwal <melvanderwal@gmail.com>
2020-10-03 10:59:51 +1000

    Add files via upload

--- js/mapbox.js
+++ js/mapbox.js
@@ -46,13 +46,12 @@ class fuelPriceData {
 
         // Get fuel types from the document cookie, or create them with default values if necessary
         this.fuelTypes = cookie.getFuelTypes();
-        if (!this.fuelTypes || !this.fuelTypes.compared || !this.fuelTypes.displayed || !this.fuelTypes.hidden) {
+        if (!this.fuelTypes) {
             this.fuelTypes = {
                 'compared': ['Unleaded', 'e10/Unleaded', 'e10'],
                 'displayed': ['Premium Unleaded 98', 'Premium Unleaded 95', 'Diesel/Premium Diesel', 'Premium Diesel', 'Diesel'],
                 'hidden': ['LPG', 'Premium e5', 'Bio-Diesel 20', 'e85', 'ULSD', 'LRP', 'OPAL', 'Compressed natural gas', 'Liquefied natural gas']
             }
-            cookie.setFuelTypes();
         }
     }   // End constructor
 
