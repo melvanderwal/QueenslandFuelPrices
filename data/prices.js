696a73430330d1756afc79762bc028264b63951a
Mel VanderWal <melvanderwal@gmail.com>
2021-03-07 18:01:04 +1000

    Scheduled prices geojson update

--- data/prices.js
+++ data/prices.js
@@ -1,7 +1,7 @@
 var data={
 	"type": "FeatureCollection",
 	"siteTime": "07/03/2021, 8:56:45 am",
-	"priceTime": "07/03/2021, 6:00:42 pm",
+	"priceTime": "07/03/2021, 6:01:02 pm",
 	"features": [
 		{
 			"type": "Feature",
